60eb710c47820275a56827efa039af9a91ea815e
Naomi Ochoa <naomi.m.ochoa@gmail.com>
2020-05-28 16:00:40 -0400

    original recursive solution for binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,23 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let middle = Math.floor(array.length /2)
+	console.log("array", array)
+	console.log("middle idx", middle)
+	console.log("middle value", array[middle])
+
+	if (array.length === 1 && array[0] !== target){
+		return false;
+	}
+	else if (array[middle] === target){
+		return true;
+	}
+	else if (array[middle] < target){
+		return binarySearch(array.slice(middle), target)
+	}
+	else {
+		return binarySearch(array.slice(0, middle), target)
+	}
 };
 
 /*
